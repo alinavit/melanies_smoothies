d21889b32ffb5d62e1cdb7bd7a7ca231f0c5931f
alinavit <65871360+alinavit@users.noreply.github.com>
2025-01-08 11:01:23 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 # Write directly to the app
 st.title("Customize Your Smoothie! :cup_with_straw:")
@@ -50,7 +51,12 @@ session = create_snowpark_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True, hide_index=True)
 st.stop()
-                                                                      
+
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
+
+
 #st.write(ingredients_list)
 #st.text(ingredients_list)
 
