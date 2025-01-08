7a98c385f452ab1750eb3890211f9ad7e4cebefc
alinavit <65871360+alinavit@users.noreply.github.com>
2025-01-08 11:05:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,11 +50,11 @@ session = create_snowpark_session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True, hide_index=True)
-st.stop()
+
 
 pd_df = my_dataframe.to_pandas()
 st.dataframe(pd_df)
-st.stop()
+
 
 
 #st.write(ingredients_list)
